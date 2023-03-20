b5bf66aec46c6a190cc6bab6fbcac34eb9ca4f8d
unknown <cap_13_96@hotmail.com>
2023-03-20 17:16:54 -0500

    Correction en muestra boton agregar

--- application/views/muestra/muestra_l.php
+++ application/views/muestra/muestra_l.php
@@ -17,16 +17,11 @@
       <h3 class="box-title"><?php echo $tablaTitulo; ?></h3>
 
       <div class="box-tools pull-right">
-        <?php 
+        <!--?php 
         if (hasPermissions('cadena_custodia_add'))
           vButton('btnNuevo', 'Nuevo', "cadena_custodia/nuevo", "fa fa-plus", "btn btn-primary");
         ?>
-        <!--button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                title="Collapse">
-          <i class="fa fa-minus"></i></button>
-        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-          <i class="fa fa-times"></i></button-->
-      </div>
+      </div-->
 
     </div>
     <div class="box-body">
